128ba34172dcd2e944ad9b27a77550a9a33d93ab
root <root@localhost.localdomain>
2018-08-25 09:52:34 +0530

    Sniffing

---
+++ snif.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+def sniffer(packet):
+    """print "Destination :",pkt[Ether].dst,"<--------->  Source : ",pkt[Ether].src
+    """
+    
+    if packet[IP].dport == 80:
+        print("\n{} ---HTTP--- {}:{}:\n{}".format(packet[IP].src,packet[IP].dst,packet[IP].dport, str(bytes(packet[TCP].payload))))
+    
+sniff(filter = 'tcp port 80', prn=sniffer)
